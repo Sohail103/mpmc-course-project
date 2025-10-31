35f4c991e4bc037f7bc747f9068a76443c5092ff
Sohail103 <sohailraj.satapathy@gmail.com>
2025-10-31 23:52:05 +0530

    shape class definition

---
+++ include/shapes.hpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+void drawline(int x1, int y1, int x2, int y2);
+
+class shape {
+
+    public:
+    std::vector<__UINT8_TYPE__> x;
+    std::vector<__UINT8_TYPE__> y;
+
+    void draw();
+}
