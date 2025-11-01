c2d5f6e0bf256e26e5cfce555e2a7f261752e888
Sohail103 <sohailraj.satapathy@gmail.com>
2025-11-01 11:02:38 +0530

    constructors and include guards

--- include/shapes.hpp
+++ include/shapes.hpp
@@ -1,20 +1,31 @@
 #include <vector>
 
+#ifndef SHAPES_H
+#define SHAPES_H
+
 void drawline(int x1, int y1, int x2, int y2);
 
-class shape {
+class Shape {
 
     public:
     std::vector<__UINT8_TYPE__> x;
     std::vector<__UINT8_TYPE__> y;
 
+    Shape(const std::vector<__UINT8_TYPE__>& xpts, const std::vector<__UINT8_TYPE__>& ypts)
+    : x(xpts), y(ypts) {};
+
     void draw();
 };
 
-class image {
+class Image {
 
     public:
-    std::vector<shape> shapes;
+    std::vector<Shape> shapes;
+
+    Image(const std::vector<Shape>& imgshapes)
+    : shapes(imgshapes) {};
 
     void draw();
 };
+
+#endif
